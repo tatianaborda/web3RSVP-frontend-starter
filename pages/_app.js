1748ea519027a5aedd928011d7881f52945dd7d4
Tatiana Borda <tatiana0borda@gmail.com>
2022-08-15 18:17:32 -0300

    displaying the event details

--- pages/_app.js
+++ pages/_app.js
@@ -5,6 +5,8 @@ import { chain, configureChains, createClient, WagmiConfig } from "wagmi";
 import { infuraProvider } from "wagmi/providers/infura";
 import { publicProvider } from "wagmi/providers/public";
 import "../styles/globals.css";
+import { ApolloProvider } from "@apollo/client";
+import client from "../apollo-client";
 
 export default function MyApp({ Component, pageProps }) {
   const infuraId = process.env.NEXT_PUBLIC_INFURA_ID;
@@ -28,9 +30,11 @@ const wagmiClient = createClient({
   return (
     <WagmiConfig client={wagmiClient}>
       <RainbowKitProvider chains={chains}>
+      <ApolloProvider client={client}>
         <Layout>
           <Component {...pageProps} />
         </Layout>
+      </ApolloProvider>
       </RainbowKitProvider>
     </WagmiConfig>
   );
